0d378126ebfb263fdf7c7f213d0e04aa8235065a
Clementine1995 <18669859508@163.com>
2020-06-23 15:26:59 +0800

    :tada: first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    <div class="nav">
+      <router-link to="/home">Home</router-link>
+      <router-link to="/user/12">User</router-link>
+      <router-link to="/desc">Desc</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view></router-view>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ src/views/User.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div>user: {{ id }}</div>
+</template>
+
+<script>
+export default {
+  name: 'user',
+  props: {
+    id: String
+  },
+  setup (...args) {
+    console.log(args)
+  }
+}
+</script>
