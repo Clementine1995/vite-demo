2ebdacb007e920a737f62279ac8c59f1b70fdfd8
Clementine1995 <18669859508@163.com>
2020-07-28 17:40:12 +0800

    :memo: 更新

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
-    <div class="nav">
-      <router-link to="/home">Home</router-link>
-      <router-link to="/user/12">User</router-link>
-      <router-link to="/desc">Desc</router-link>
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view></router-view>
+  <div class="nav">
+    <router-link to="/home">Home</router-link>
+    <router-link to="/user/12">User</router-link>
+    <router-link to="/desc">Desc</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view></router-view>
 </template>
 
-<script>
+<script lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
