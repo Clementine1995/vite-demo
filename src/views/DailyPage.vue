3028b79641e5dfe8cb7d9cc95d36f0f1b7c2e825
huyichen <335086@tgood.com>
2022-06-28 11:08:25 +0800

    :art: update

--- src/views/DailyPage.vue
+++ src/views/DailyPage.vue
@@ -1,51 +1,13 @@
 <template>
   <div>
-    <a-table :columns="columns" :data-source="data">
-      <template #headerCell="{ column }">
-        <template v-if="column.key === 'name'">
-          <span> Name </span>
-        </template>
-      </template>
-
-      <template #bodyCell="{ column, record }">
-        <template v-if="column.key === 'name'">
-          <a>
-            {{ record.name }}
-          </a>
-        </template>
-        <template v-else-if="column.key === 'tags'">
-          <span>
-            <a-tag
-              v-for="tag in record.tags"
-              :key="tag"
-              :color="
-                tag === 'loser'
-                  ? 'volcano'
-                  : tag.length > 5
-                  ? 'geekblue'
-                  : 'green'
-              "
-            >
-              {{ tag.toUpperCase() }}
-            </a-tag>
-          </span>
-        </template>
-        <template v-else-if="column.key === 'action'">
-          <span>
-            <a>Invite 一 {{ record.name }}</a>
-            <a-divider type="vertical" />
-            <a>Delete</a>
-          </span>
-        </template>
-      </template>
-    </a-table>
+    <a-table :columns="columns" :data-source="data"> </a-table>
   </div>
 </template>
 <script lang="ts">
   import { defineComponent } from 'vue'
   const columns = [
     {
-      name: 'Name',
+      title: 'Name',
       dataIndex: 'name',
       key: 'name',
     },
@@ -59,15 +21,6 @@
       dataIndex: 'address',
       key: 'address',
     },
-    {
-      title: 'Tags',
-      key: 'tags',
-      dataIndex: 'tags',
-    },
-    {
-      title: 'Action',
-      key: 'action',
-    },
   ]
 
   const data = [
@@ -76,21 +29,18 @@
       name: 'John Brown',
       age: 32,
       address: 'New York No. 1 Lake Park',
-      tags: ['nice', 'developer'],
     },
     {
       key: '2',
       name: 'Jim Green',
       age: 42,
       address: 'London No. 1 Lake Park',
-      tags: ['loser'],
     },
     {
       key: '3',
       name: 'Joe Black',
       age: 32,
       address: 'Sidney No. 1 Lake Park',
-      tags: ['cool', 'teacher'],
     },
   ]
 
